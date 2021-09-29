f55d040d063e22f98aa5a042cef28b87a90cede6
AlvaroRecioRubio <91250827+AlvaroRecioRubio@users.noreply.github.com>
2021-09-29 09:19:28 +0200

    Add files via upload

---
+++ Práctica definitiva1.ps1
@@ -0,0 +1,39 @@
+﻿
+write-host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+write-host "|                               |"
+Write-Host "|           ░░░▓▓▓▓▓░░░░        |"
+write-host "|           ░░▓▓▓▓▓▓▓▓▓░        |"
+write-host "|           ░░███▒▒█▒░░░        |"
+write-host "|           ░█▒██▒▒▒█▒▒▒        |"
+write-host "|           ░██▒▒▒▒████░        |"
+write-host "|           ░░░▒▒▒▒▒▒▒░░        |"
+write-host "|           ░░██▓███░░░░        |"
+write-host "|           ░███▓██▓███░        |"
+write-host "|           ████▓▓▓▓████        |"
+write-host "|           ▒▒█▓▒▓▓▒▓█▒▒        |"
+write-host "|           ▒▒▒▓▓▓▓▓▓▒▒▒        |"
+write-host "|           ░░▓▓▓░░▓▓▓░░        |"
+write-host "|           ░███░░░░███░        |"
+write-host "|           ████░░░░████        |"
+write-host "|            menu               |"
+write-host "|                               |"
+write-host "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+
+write host "1. Convierte de binario a decimal"
+write host "2. convierte de hexadecimal a binario"
+$option = read-host "selecciona operación"
+
+if ($option -eq 1)
+{
+    $binario = Read-host "dime el número binario"
+    $valor =[convert ]::toint32($binario,2)
+    "el número decimal es..." + $valor
+}
+
+if ($option -eq 2)
+{
+    $hexa = read-host "dime el número en hexadecimal"
+    $valor = [convert]::toint32($hexa,16)
+    $valor = [convert]::ToString($valor,2)
+    "El valor en hexadecimal es..." + $valor
+}
